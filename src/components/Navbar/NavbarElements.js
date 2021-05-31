0bfa06bade147300d01784bc33825f83e20dbc52
carbondesigned <dylan.reeder5@gmail.com>
2021-05-31 16:38:29 -0400

    finished structuring nav

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -4,8 +4,8 @@ import { Link as LinkS } from "react-scroll"
 
 export const Nav = styled.nav`
   background-color: black;
-  padding: 2em;
-  margin: -80px;
+  padding: 1em 2em;
+  /* margin: -80px; */
   display: flex;
   justify-content: center;
   align-items: center;
@@ -22,7 +22,7 @@ export const Nav = styled.nav`
 export const NavbarContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  /* padding: 2em; */
+  align-items: center;
   z-index: 1;
   width: 100%;
   max-width: 1100px;
@@ -38,4 +38,78 @@ export const NavLogo = styled(LinkR)`
   margin-left: 2em;
   font-weight: bold;
   text-decoration: none;
+`
+
+export const MobileIcon = styled.div`
+  display: none;
+  color: #fff;
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 55%);
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+`
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  text-align: center;
+  margin-right: -22px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+export const NavItems = styled.li`
+  padding: 1em 0;
+`
+
+export const NavLinks = styled(LinkS)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1em;
+  height: 100%;
+  cursor: pointer;
+
+  &.active {
+    border-bottom: 0.2em solid #01bf71;
+  }
+`
+
+export const NavBtn = styled.nav`
+  display: flex;
+  align-items: center;
+
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+export const NavBtnLink = styled(LinkR)`
+  border-radius: 20em;
+  background: transparent;
+  white-space: nowrap;
+  padding: 0.85em 3em;
+  color: #fff;
+  font-size: 1em;
+  outline: none;
+  border: #01bf71 solid 0.2em;
+  cursor: pointer;
+  transition: 500ms;
+  text-decoration: none;
+  margin-left: 2em;
+
+  &:hover {
+    transition: 500ms;
+    background: #01bf71;
+    color: #010606;
+  }
 `
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,47 @@
 import React from "react"
 
+// Icons
+import { FaBars } from "react-icons/fa"
+
 // Import full navbar
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements"
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItems,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements"
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo>Dolla</NavLogo>
+          <NavLogo to="/">Dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItems>
+              <NavLinks to="about">About</NavLinks>
+            </NavItems>
+            <NavItems>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItems>
+            <NavItems>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItems>
+            <NavItems>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItems>
+            <NavBtn>
+              <NavBtnLink to="/signin">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
